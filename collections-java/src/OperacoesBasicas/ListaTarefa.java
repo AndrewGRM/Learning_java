fbb834a11a9eab65fc3db9ea1c2cecffd633fceb
AndrewGRM <andrew.grm15@gmail.com>
2025-06-18 19:29:50 -0300

    Resolvendo desafio
    Signed-off-by: AndrewGRM <andrew.grm15@gmail.com>

---
+++ collections-java/src/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,50 @@
+package OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefalist;
+
+    public ListaTarefa() {
+        this.tarefalist = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefalist.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefalist) {
+            if(t.getDescricao().equals(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefalist.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalDeTarefas() {
+        return tarefalist.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefalist);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de tarefas na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+
+        System.out.println("O número total de tarefas na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+        //listaTarefa.removerTarefa("Tarefa 3");
+        System.out.println("O número total de tarefas na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+        listaTarefa.obterDescricoesTarefas();
+    }
+}
